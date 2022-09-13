76ee342b313f63d049d2d8b0e2dd13743c2483ae
Guilherme da Rocha Spanserki <guilherme.spanserki@cas.ilumisol.com.br>
2022-09-13 15:47:15 -0300

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>
+        Hello word!
+        </Text>
+        <Button title='Enviar'/>
+      <StatusBar style="auto"/>
     </View>
   );
+}
+
+interface buttonProps {
+  title: string;
+}
+
+function Button({title}: buttonProps) {
+  return (
+    <TouchableOpacity>
+      <Text style={styles.button}>
+        {title}
+      </Text>
+    </TouchableOpacity>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: '#fff',
+    fontSize: 20
+  },
+  button: {
+    backgroundColor: '#fff',
+    marginTop: 20,
+    padding: 10,
+    borderRadius: 8
+  }
 });
